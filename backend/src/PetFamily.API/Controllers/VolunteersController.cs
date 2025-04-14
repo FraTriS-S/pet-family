c83e3f24ac70a592359eb57efcb7753c8b6218da
Zelyanyakas Denis <brotok2001@yandex.ru>
2025-04-14 22:03:34 +0700

    feat(PetFamily.API): привел проект в нужный вид, добавил контреллер в котором есть метод создания волонтера.

---
+++ backend/src/PetFamily.API/Controllers/VolunteersController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using PetFamily.API.Extensions;
+using PetFamily.Application.Volunteers.CreateVolunteer;
+
+namespace PetFamily.API.Controllers;
+
+public class VolunteersController : ApplicationController
+{
+    [HttpPost]
+    public async Task<ActionResult<Guid>> Create(
+        [FromServices] CreateVolunteerHandler handler,
+        [FromBody] CreateVolunteerRequest request,
+        CancellationToken cancellationToken)
+    {
+        var result = await handler.HandleAsync(request, cancellationToken);
+
+        return result.ToResponse();
+    }
+}
